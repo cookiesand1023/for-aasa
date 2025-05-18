5fadf1a79a25e6fd6a1b25745731616d09ea5360
kuki_kyosuke <>
2025-05-19 01:50:03 +0900

    fixed

--- src/app/api/.well-known/apple-app-site-association/route.ts
+++ src/app/api/.well-known/apple-app-site-association/route.ts
@@ -6,11 +6,8 @@ export function GET() {
       details: [
         {
           appIDs: ['484Z9E6GB6.com.youpacefans.app'],
-          components: [
-            {
-              '/': '/invitation/*',
-              comment: 'Matches invitation links to open in the app',
-            },
+          paths: [
+            '/invitation/*',
           ],
         },
       ],
