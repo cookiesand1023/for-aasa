5feb990b2bb2b7b58c96d1f2d0380c57613cd8ed
kuki_kyosuke <>
2025-05-27 14:28:37 +0900

    file

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: '/.well-known/apple-app-site-association',
         destination: '/api/.well-known/apple-app-site-association',
-      },
-      {
-        source: '/.well-known/assetlinks.json',
-        destination: '/api/.well-known/assetlinks.json',
-      },
+      }
     ];
   },
 };
